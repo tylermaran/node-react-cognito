12ecbde6e5eba32073e99fae5b88a6b325301d21
themanmaran <tyler.maran@gmail.com>
2019-11-07 21:28:07 -0800

    added api call to comment box

--- client/src/pages/Comments.jsx
+++ client/src/pages/Comments.jsx
@@ -7,16 +7,44 @@ import NavBar from '../components/NavBar';
 // Importing Styling
 import './Comments.css';
 
-const Comments = props => {
-	return (
-		<div className="Comments">
-			<NavBar />
-			<div className="container_lg">
-				<div className="header">Comments</div>
-				<div className="container_body"></div>
+class Comments extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			comments: '',
+		};
+	}
+	componentDidMount() {
+		fetch(process.env.REACT_APP_API_URL + 'users')
+			.then(result => {
+				return result.json();
+			})
+			.then(data => {
+				console.log(data);
+				let temp = data.map(this.map_comments);
+				this.setState({
+					comments: temp,
+				});
+			});
+	}
+	map_comments = data => {
+		return (
+			<div className="comment_div" key={Math.random() * 9}>
+				<div className="comment_body">{data.email}</div>
 			</div>
-		</div>
-	);
-};
+		);
+	};
+	render() {
+		return (
+			<div className="Comments">
+				<NavBar />
+				<div className="container_lg">
+					<div className="header">Comments</div>
+					<div className="container_body">{this.state.comments}</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Comments;
